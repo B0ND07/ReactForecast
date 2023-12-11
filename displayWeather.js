de626b27a13d6592c7fb84aa1dee26f1224b6d38
b0nd07 <mhashimkp@gmail.com>
2023-12-11 23:34:37 +0530

    ReactForecast: add icons from api - use based on weather condition

--- displayWeather.js
+++ displayWeather.js
@@ -6,6 +6,7 @@ import {
   WiHumidity,
   WiBarometer,
 } from "react-icons/wi";
+import { FaCloud, FaCloudSun, FaCloudMoon, FaCloudShowersHeavy, FaSun, FaMoon, FaCloudRain, FaCloudSunRain, FaCloudMoonRain, FaBolt, FaSnowflake, FaSmog } from 'react-icons/fa';
 
 function WeatherDisplay({ weatherData }) {
   function formatTime(timestamp) {
@@ -19,12 +20,49 @@ function WeatherDisplay({ weatherData }) {
     return temperature > 30 ? '#e74c3c' : '#ffc107'; // Change color based on temperature
   };
 
+  const getWeatherIcon = (iconCode) => {
+    switch (iconCode) {
+      case "01d":
+        return <FaSun />;
+      case "01n":
+        return <FaMoon />;
+      case "02d":
+        return <FaCloudSun />;
+      case "02n":
+        return <FaCloudMoon />;
+      case "03d":
+      case "03n":
+        return <FaCloud />;
+      case "04d":
+      case "04n":
+        return <FaCloud />;
+      case "09d":
+      case "09n":
+        return <FaCloudShowersHeavy />;
+      case "10d":
+        return <FaCloudSunRain />;
+      case "10n":
+        return <FaCloudMoonRain />;
+      case "11d":
+      case "11n":
+        return <FaBolt />;
+      case "13d":
+      case "13n":
+        return <FaSnowflake />;
+      case "50d":
+      case "50n":
+        return <FaSmog />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       {weatherData.map((display) => (
         <div key={display.id} className="weather-details">
           <h1 className="city-name">
-            {display.name}, {display.sys.country} ({display.weather[0].description})
+            {display.name}, {display.sys.country} ({display.weather[0].description} {getWeatherIcon(display.weather[0].icon)})
           </h1>
           
           <p className="temperature" style={{ color: temperatureColor(display.main.temp) }}>
